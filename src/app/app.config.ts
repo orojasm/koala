5ec84c47ca759135eaa59758e661ac52440c4395
Oscar Rojas <orojas.molina@gmail.com>
2024-04-24 18:37:59 -0500

    chore(configuration): :tada: firebase installation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,11 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '@env/environment.development';
+
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateLoader, TranslateModule, TranslateModuleConfig } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
@@ -23,6 +28,15 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(HttpClientModule),
-    importProvidersFrom( TranslateModule.forRoot( translateModuleConfig ) ),
+    importProvidersFrom(TranslateModule.forRoot(translateModuleConfig)),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebase))
+    ),
+    importProvidersFrom(
+      provideAuth(() => getAuth())
+    ),
+    importProvidersFrom(
+      provideFirestore(() => getFirestore())
+    ),
   ]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,12 @@
 export const environment = {
   apiUrl: '',
   production: true,
+  firebase: {
+    projectId: "koala-e153d",
+    appId: "1:985816253475:web:f629ae9195ec358acf5d96",
+    storageBucket: "koala-e153d.appspot.com",
+    apiKey: "AIzaSyDI16BU9w_-FBbaiRNSIBOy5-YX1LEJ7Hw",
+    authDomain: "koala-e153d.firebaseapp.com",
+    messagingSenderId: "985816253475"
+  },
 };
